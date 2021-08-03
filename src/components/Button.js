b5ffdf406c0337ac48fddd00240becd5399b5854
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-03 13:32:29 -0400

    add condition so prop updates buttonClass

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,6 +3,15 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <button>{props.children}</button>
-   ;
+   let buttonClass = "button";
+
+   if (props.confirm) {
+      buttonClass += " button--confirm";
+   } 
+   
+   if (props.danger) {
+      buttonClass += " button--danger";
+   }
+
+   return <button className={buttonClass}>{props.children}</button>;
 }
