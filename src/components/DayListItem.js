fce2c12dbb922f7feded7e273d6fc36e2fa32d13
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-03 14:48:06 -0400

    refactor code to specified

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,13 +1,11 @@
 import React from "react";
 
 export default function DayListItem(props) {
+
   return (
-    // <li> represents the entire day item
-    <li> 
-      {/* <h2> should display the day name </h2>   */}
-      <h2 className="text--regular">Day Name</h2> 
-      {/* <h3> should display the spots remaining for a day </h3> */}
-      <h3 className="text--light">X spots remaining</h3>
+    <li onClick={() => {props.setDay(props.name)}}> 
+      <h2 className='text--regular'>{props.name}</h2> 
+      <h3 className='text--light'>{props.spots}</h3>
     </li>
   );
 }
