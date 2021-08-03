b1305b7a5aed9bdb51c8798cfb9fc1ef41c7792d
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-03 13:40:00 -0400

    add onLick and disabled prop to button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import "components/Button.scss";
+import { action } from "@storybook/addon-actions/dist/preview";
 
 export default function Button(props) {
    let buttonClass = "button";
@@ -13,9 +14,13 @@ export default function Button(props) {
       buttonClass += " button--danger";
    }
 
-   if (props.disabled) {
-      buttonClass += " button-disabled";
-   }
-
-   return <button className={buttonClass}>{props.children}</button>;
+   return (
+      <button 
+         className={buttonClass}
+         onClick={props.onClick}
+         disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+      );
 }
