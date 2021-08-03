a1c6462a9b4c72eb243b795da3cf962e9039fc4d
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-03 14:15:11 -0400

    add comments

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function DayListItem(props) {
   return (
-    <li>
+    // <li> represents the entire day item
+    <li> 
+      {/* <h2> should display the day name </h2>   */}
       <h2 className="text--regular">Day Name</h2> 
+      {/* <h3> should display the spots remaining for a day </h3> */}
       <h3 className="text--light">X spots remaining</h3>
     </li>
   );
