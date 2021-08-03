ce3d0021ce9f4257dcc1925a78f2ceb27032ec7c
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-03 14:25:05 -0400

    add into index.js

--- src/index.js
+++ src/index.js
@@ -7,15 +7,3 @@ import Application from "components/Application";
 
 ReactDOM.render(<Application />, document.getElementById("root"));
 
-storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
-  .addParameters({
-    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
-  }) // Provides the default background color for our component
-  // To define our stories, we call add() once for each of our test states to generate a story
-  .add("Unselected", () => <DayListItem name="Monday" spots={5} />) 
-  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
-  .add("Full", () => <DayListItem name="Monday" spots={0} />)
-  .add("Clickable", () => (
-    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
-  ));
-
