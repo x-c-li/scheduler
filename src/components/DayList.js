4840fbcf55c785d6503ab06bf94f0691bb713d79
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-03 15:37:54 -0400

    create DayList.js and return empty <ul></ul>

---
+++ src/components/DayList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayList.scss";
+
+export default function DayList(props) {
+  
+  // const DayListClass = classNames("day-list__item", {
+  //   '--selected': props.selected,
+  //   '--full': props.spots
+  // });
+
+  return (
+    <ul></ul>
+  );
+}
