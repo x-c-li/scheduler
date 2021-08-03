da399babbb2c697860e605cc5b08640ad9de4751
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-03 16:16:25 -0400

    adjust CSS

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -5,14 +5,14 @@ import "components/DayListItem.scss";
 export default function DayListItem(props) {
   
   const DayListClass = classNames("day-list__item", {
-    '--selected': props.selected,
-    '--full': props.spots
+    'day-list__item--selected': props.selected,
+    'day-list__item--full': props.spots === 0
   });
 
   return (
     <li className={DayListClass} onClick={() => {props.setDay(props.name)}}> 
-      <h2>{props.name}</h2> 
-      <h3>{formatSpots(props.spots)}</h3>
+      <h2 className='text--regular'>{props.name}</h2> 
+      <h3 className='text--light'>{formatSpots(props.spots)}</h3>
     </li>
   );
 }
