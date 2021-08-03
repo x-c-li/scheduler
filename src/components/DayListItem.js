845552713c9d402941dca9d50c732bfad85c0807
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-03 15:12:34 -0400

    add classNames to make CSS work

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,18 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  
+  const DayListClass = classNames("day-list__item", {
+    '--selected': props.selected,
+    '--full': props.spots
+  });
 
   return (
-    <li onClick={() => {props.setDay(props.name)}}> 
-      <h2 className='text--regular'>{props.name}</h2> 
-      <h3 className='text--light'>{props.spots}</h3>
+    <li className={DayListClass} onClick={() => {props.setDay(props.name)}}> 
+      <h2>{props.name}</h2> 
+      <h3>{props.spots}</h3>
     </li>
   );
 }
