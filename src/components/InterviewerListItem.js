156746e07638ac813cbfa17f64c5a496cfb90afd
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-03 20:32:20 -0400

    add className var, and onclick event

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import "components/InterviewerListItem.scss";
+import classNames from  'classnames';
 
+export default function InterviewerListItem(props) {
+  
+  const InterviewerListClass = classNames("interviewers__item", {
+    'interviewers__item--selected': props.selected,
+  });
 
-export default function Component() {
   return (
-    <li className="interviewers__item">
-      <img
-        className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
-      />
-      Sylvia Palmer
-    </li>
+    <div>
+      <li className={InterviewerListClass} onClick={() => {props.setInterviewer(props.name)}}>
+        <img
+          className='interviewers__item-image'
+          src={props.avatar}
+          alt={props.name}
+        />
+        {props.selected && props.name}
+      </li>
+    </div>
   );
 }
