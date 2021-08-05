4471dc9be3e5c567c05c87f029a3e1172916718c
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-05 17:56:28 -0400

    write function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,2 +1,27 @@
 //selector is to help compute new data from existing state in app 
 
+import React from "react";
+
+export function getAppointmentsForDay(state, day) {
+
+  const daysArray = state.days
+  let dayAppointments = false;
+  const results = [];
+
+  for (const item of daysArray) {
+    if (item.name === day) {
+      dayAppointments = [...item.appointments]
+    }
+  }
+
+  if (dayAppointments) {
+    for (const key in state.appointments) {
+      if (dayAppointments.includes(state.appointments[key].id)) {
+        results.push(state.appointments[key]);
+      }
+
+    }
+  }
+
+  return results;
+}
