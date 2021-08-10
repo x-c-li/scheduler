a3124df855dc7e428ed25ce791f027fbbb50267f
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-10 14:03:41 -0400

    check code against answer

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -29,30 +29,44 @@ describe("Form", () => {
     expect(getByTestId("student-name-input")).toHaveValue("Lydia Miller-Jones");
   });
 
-  t("validates that the student name is not blank", () => {
+  it("validates that the student name is not blank", () => {
     /* 1. Create the mock onSave function */
-    fireEvent.click(getByText("Save"));
-    
+    const onSave = jest.fn()
+
     /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the name prop should be blank or undefined */
-  
+    const {getByText} = render(
+      <Form 
+        interviewers={interviewers}
+        onSave={onSave} 
+      />) 
+
     /* 3. Click the save button */
-  
+    fireEvent.click(getByText("Save"));
+
     expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
     expect(onSave).not.toHaveBeenCalled();
   });
   
   it("calls onSave function when the name is defined", () => {
     /* 1. Create the mock onSave function */
-    
+    const onSave = jest.fn()
+
     /* 2. Render the Form with interviewers, name and the onSave mock function passed as an onSave prop */
-  
+    const {getByText, queryByText} = render(
+      <Form 
+        interviewers={interviewers}
+        onSave={onSave} 
+        name="Lydia Miller-Jones"
+      />) 
     /* 3. Click the save button */
+    fireEvent.click(getByText("Save"));
+
   
-    /* 3. validation is not shown */
+    /* validation is not shown */
     expect(queryByText(/student name cannot be blank/i)).toBeNull();
-    /* 4. onSave is called once*/
+    /* onSave is called once*/
     expect(onSave).toHaveBeenCalledTimes(1);
-    /* 5. onSave is called with the correct arguments */
+    /* onSave is called with the correct arguments */
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
   });
 });
