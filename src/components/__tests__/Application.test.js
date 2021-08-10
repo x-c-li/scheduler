b3f3563b53f0e0f650342579560f126a23c926a1
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-10 18:39:45 -0400

    put all tests in describe block, copy in test and steps to take in test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -3,6 +3,7 @@ import React from "react";
 import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
+import { forceReRender } from "@storybook/react";
 
 afterEach(cleanup);
 
@@ -14,14 +15,41 @@ afterEach(cleanup);
 //     expect(getByText("Leopold Silvers")).toBeInTheDocument();
 //   });
 // });
+describe("Application", () => {
 
-it("changes the schedule when a new day is selected", async () => {
-  const { getByText } = render(<Application />);
-  //asynchronous function has been defined as one using the async keyword.
-  // Promise chain can be hidden by using the await keyword
-  await waitForElement(() => getByText("Monday"));
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+    //asynchronous function has been defined as one using the async keyword.
+    // Promise chain can be hidden by using the await keyword
+    await waitForElement(() => getByText("Monday"));
+  
+    fireEvent.click(getByText("Tuesday"));
+  
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+  
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    // Render the Application.
+    const { getByText } = render(<Application />);
+    
+    // Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText("Monday"));
+    fireEvent.click(getByText("Monday"))
+  
+    ex[ect]
+    
+    // Click the "Add" button on the first empty appointment.
+    // Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".
+    // Click the first interviewer in the list.
+    // Click the "Save" button on that same appointment.
+    // Check that the element with the text "Saving" is displayed.
+    // Wait until the element with the text "Lydia Miller-Jones" is displayed.
+  
+  
+    // Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  
+  })
 
-  fireEvent.click(getByText("Tuesday"));
+})
 
-  expect(getByText("Leopold Silvers")).toBeInTheDocument();
-});
