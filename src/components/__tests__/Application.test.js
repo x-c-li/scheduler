c887fa6c799a02f592d26d84f6978d8b991aa63a
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-10 21:03:18 -0400

    refactor code to compass standards

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -28,25 +28,26 @@ describe("Application", () => {
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
     const appointments = getAllByTestId(container, "appointment");
-
-    const appointment = getAllByTestId(container, "appointment")[0];
+    const appointment = appointments[0];
 
     fireEvent.click(getByAltText(appointment, "Add"));
+    
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" }
     });
   
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
-  
     fireEvent.click(getByText(appointment, "Save"));
 
     //updating appointment, if saves, we'll see saving 
-    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
 
     const day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
     );
-    
+
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+
   })
 
 })
