992b9efeac6f3430f8a5579c56a9b34feddcbfd7
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-11 20:45:42 -0400

    Add the command to select the interviewer with the name "Sylvia Palmer".

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -2,17 +2,19 @@ describe("Navigation", () => {
   
   it("should book an interview", () => {
     cy
-      .visit("/")// Visits the root of our web server
-      .contains("[data-testid=day]", "Monday")//check dom for text Monday
+    .visit("/")// Visits the root of our web server
+    .contains("[data-testid=day]", "Monday")//check dom for text Monday
     
     cy // Clicks on the "Add" button in the second appointment
-      .get("[alt=Add")
-      .first()
-      .click();
+    .get("[alt=Add")
+    .first() //the second one is for the 5pm one
+    .click();
 
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones"); // Enters their name
 
+    cy.get("[alt='Sylvia Palmer']").click(); // click on interviewer Sylvia Palmer
 
+    
 
   });
 
