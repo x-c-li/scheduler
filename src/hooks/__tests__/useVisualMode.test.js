3a7ef9c1f31b4a13aced9cc4c6cd5b2d753d2379
Xiaowei (Cathy) Li <xiaowei.c.li@gmail.com>
2021-08-14 17:01:49 -0400

    changing file paths

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -1,6 +1,6 @@
 import { renderHook, act } from "@testing-library/react-hooks";
 
-import useVisualMode from "hooks/useVisualMode";
+import useVisualMode from "../useVisualMode";
 
 
 const FIRST = "FIRST";
